3dc64f5e71760ebccca5c8e410d481cde604d5f2
Konstantin Dvornichenko <knstd@rambler.ru>
2022-11-08 11:40:00 +0400

    Homework 2.2 m2m-relations/

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Relationship, Tag
+
+
+class RelationshipInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        main_tag = 0
+        for form in self.forms:
+            for key, value in form.cleaned_data.items():
+                if key == 'is_main' and value:
+                    main_tag += 1
+        if main_tag > 1:
+            raise ValidationError('Не может быть двух главных разделов')
+        return super().clean()
+
+
+class RelationshipInline(admin.TabularInline):
+    model = Relationship
+    formset = RelationshipInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [RelationshipInline]
+    list_display = ('id', 'title', 'text', 'published_at', 'image')
+    list_display_links = ('title',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
